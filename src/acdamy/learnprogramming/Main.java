43e8360cc2f9e2d3b028770bdd079e14fb0fdd35
sudip34 <sudipcep2006@gmail.com>
2021-04-10 14:01:38 +0200

    2nd Commit

--- src/acdamy/learnprogramming/Main.java
+++ src/acdamy/learnprogramming/Main.java
@@ -1,5 +1,8 @@
 package acdamy.learnprogramming;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,34 +11,29 @@ public class Main {
 
     public static void main(String[] args) {
         String str1="sudip@gmail.com";
-        String str2="1234Q567";
+        String str3="sudip34@gmail.com";
+
+
+        List<String>emailList=new ArrayList<>();
+        emailList.add(str1);
+        emailList.add(str3);
+
         System.out.println("Enter your Email :");
         Scanner scanner = new Scanner(System.in);
         String sct1=scanner.nextLine();
 
-        System.out.println("Enter your password:");
-        Scanner scanner2 = new Scanner(System.in);
-        String sct2=scanner2.nextLine();
-
-
-        Pattern pattern=Pattern.compile(str1);
-        Matcher matcher1=pattern.matcher(sct1);
-
-        Pattern pattern2=Pattern.compile(str2);
-        Matcher matcher2=pattern2.matcher(sct2);
-
-
-
-        if (matcher1.matches()&&matcher2.matches()){
-            System.out.println("you are in the system and your Email : "+str1);
-        }else{
-            if(matcher1.matches()) {
-                System.out.println("Wrong password.");
-            }else {
-                System.out.println("wrong Email address.");
-                System.out.println("Please enter your correct email address.");
-
+        boolean isExist=false;
+        for (int i=0; i < emailList.size(); i++) {
+            if (sct1.equals( emailList.get(i))) {
+                isExist=true;
+                break;
             }
         }
+        if (isExist){
+            System.out.println(sct1+" is found.");
+        }else{
+            System.out.println(sct1+" not found");
+        }
+        
     }
 }
